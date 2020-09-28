3826b8b28a5a7c9f88332c0303004328fce8f025
Glitch (rain-attractive-slope) <none>
2020-09-28 08:48:58 +0000

    💪☂️ Checkpoint ./yardım.js:13663528/1307 ./komutlar/wasted.js:13663528/2488

--- komutlar/wasted.js
+++ komutlar/wasted.js
@@ -1,37 +1,38 @@
-var Jimp = require('jimp');
 const Discord = require('discord.js');
 
-module.exports.run = async (bot, message, args) => {
-
-  var user = message.mentions.users.first() || message.author;
-      message.channel.startTyping();
-        var user = message.mentions.users.first() || message.author;
-        if (!message.guild) user = message.author;
-
-        Jimp.read(user.avatarURL, (err, image) => {
-            image.resize(295, 295)
-            image.greyscale()
-            image.gaussian(3)
-            Jimp.read("https://cdn.glitch.com/b18a2fa6-68cb-49d5-9818-64c50dd0fdab%2F1.png?1529363616039", (err, avatar) => {
-                avatar.resize(295, 295)
-                image.composite(avatar, 4, 0).write(`./img/wasted/${bot.user.id}-${user.id}.png`);
-                setTimeout(function() {
-                    message.channel.send(new Discord.Attachment(`./img/wasted/${bot.user.id}-${user.id}.png`));
-                }, 1000);
-          message.channel.stopTyping();
-            });
-        });
-    };
-
-exports.conf = {
+exports.run = async (client, message, params) => {
+    if(message.channel.type == "dm")  return;
+  if(message.channel.type !== "text") return;
+  var channel = client.channels.find('id', '540269834835853340')
+    const asdf = await client.channels.get(message.channel.id).createInvite()
+  message.delete();
+  const embed = new Discord.RichEmbed()
+  .setTitle("»  Yardım Komutunu Kullandınız")
+  .setDescription("**» a!yardım genel**")
+  .setDescription("**» a!yardım eğlence**")
+  .setDescription("**» a!yardım kayıt**")
+  .setDescription("**» a!yardım sunucu**")
+  .setDescription("**» Daha Fazla Yardım için Aşağıdaki Discord Sunucusuna Katıl!**")
+  .setURL("https://discord.gg/VsVmD5d")
+  .setFooter("Yukarıdaki Sunucuda Daha Fazla Yardım Alabilirsin")
+ message.channel.send(embed)
+      const invite = new Discord.RichEmbed()
+  .setAuthor("» Canlı Destek | Talep")
+  .addField('**» Kullanıcı Adı: **', message.author.username + '#' + message.author.discriminator)
+  .addField('**» Sunucu Adı: **', message.guild.name)
+  .setDescription(asdf.url)
+      channel.send(invite)
+};
+  
+  exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ['help','yardım'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'wasted',
-  description: 'wasted.',
-  usage: 'wasted'
+  name: 'yardım',
+  description: 'yardım',
+  usage: 'yardım'
 };
